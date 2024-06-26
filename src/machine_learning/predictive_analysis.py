0fdf256f3c31614751b717385147bad5f94d1c5f
OV00VO V <oskar.d.vikstrom@gmail.com>
2024-06-26 20:05:22 +0000

    Heroku issue still too large deployment, install apps for running, import tensorflow

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -5,6 +5,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.express as px
+import tflite as tf
 from keras.models import load_model
 from PIL import Image
 from src.data_management import load_pkl_file

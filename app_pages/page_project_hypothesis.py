00c203d5259f6e767932f851bedd52fd9e76dfe4
OV00VO V <oskar.d.vikstrom@gmail.com>
2024-06-26 20:30:42 +0000

    Heroku issue still too large deployment, install apps for running, change in ml_performance for tensorflow

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -10,9 +10,9 @@ def page_project_hypothesis_body():
 
     st.success(
         f"* We suspect mildew on the cherry leaf and it have clear marks/signs, "
-        f"typically on small parts of the leaf, that can differentiate them from an uninfected leaf. \n\n"
+        f"typically on small parts of the leaf at first, that can differentiate them from an uninfected leaf. \n\n"
         f"* An Image Montage shows that typically a mildew leaf has powdery mildew across it. "
-        f"Average Image, Variability Image and Difference between Averages studies did not reveal "
-        f"any clear pattern to differentiate one from another."
+        f"Average Image, Variability Image and Difference between Averages studies did reveal "
+        f"a clear pattern to differentiate one from another."
 
     )
--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -5,8 +5,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import tflite as tf
-from keras.models import load_model
+import tensorflow
+from tensorflow.keras.models import load_model
 from PIL import Image
 from src.data_management import load_pkl_file
 
